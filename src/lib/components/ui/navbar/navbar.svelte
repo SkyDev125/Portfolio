291168c7e895d58184674780f0c4ec073c4de783
Sky <diogo_goncalves2004@hotmail.com>
2025-10-02 00:53:55 +0100

    feat: add navbar

---
+++ src/lib/components/ui/navbar/navbar.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
+	import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+	import { resetMode, setMode } from 'mode-watcher';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+</script>
+
+<nav class="flex justify-end p-4">
+	<NavigationMenu.Root>
+		<NavigationMenu.List>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link>
+					{#snippet child()}
+						<a href="/" class={navigationMenuTriggerStyle()}>Home</a>
+					{/snippet}
+				</NavigationMenu.Link>
+			</NavigationMenu.Item>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link>
+					{#snippet child()}
+						<a href="/about" class={navigationMenuTriggerStyle()}>About</a>
+					{/snippet}
+				</NavigationMenu.Link>
+			</NavigationMenu.Item>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link>
+					{#snippet child()}
+						<a href="/projects" class={navigationMenuTriggerStyle()}>Projects</a>
+					{/snippet}
+				</NavigationMenu.Link>
+			</NavigationMenu.Item>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link>
+					{#snippet child()}
+						<a href="/contact" class={navigationMenuTriggerStyle()}>Contact</a>
+					{/snippet}
+				</NavigationMenu.Link>
+			</NavigationMenu.Item>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link>
+					{#snippet child()}
+						<a href="/skills" class={navigationMenuTriggerStyle()}>Skills</a>
+					{/snippet}
+				</NavigationMenu.Link>
+			</NavigationMenu.Item>
+			<NavigationMenu.Item>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger class={buttonVariants({ variant: 'outline', size: 'icon' })}>
+						<SunIcon
+							class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+						/>
+						<MoonIcon
+							class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+						/>
+						<span class="sr-only">Toggle theme</span>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content align="end">
+						<DropdownMenu.Item onclick={() => setMode('light')}>Light</DropdownMenu.Item>
+						<DropdownMenu.Item onclick={() => setMode('dark')}>Dark</DropdownMenu.Item>
+						<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</NavigationMenu.Item>
+		</NavigationMenu.List>
+	</NavigationMenu.Root>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import Navbar from '$lib/components/ui/navbar/navbar.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<!-- Mode Watcher to handle theme changes -->
+<ModeWatcher />
+
+<!-- Navigation Menu -->
+<Navbar />
+
+<!-- Main Content -->
+{@render children?.()}
